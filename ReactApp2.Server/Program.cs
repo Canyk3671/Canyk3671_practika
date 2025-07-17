3167c1ae7a518053ee16dc4d43f8ae323a656390
Ryabinin Aleksandr <sasha.ryabinin.04@mail.ru>
2025-07-17 12:51:20 +0300

    Initial commit

--- ReactApp2.Server/Program.cs
+++ ReactApp2.Server/Program.cs
@@ -1,33 +1,23 @@
+﻿using System.Diagnostics.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<List<Measurement>>(new List<Measurement>());
+
 var app = builder.Build();
 
-app.UseRequestLocalization("ru-Ru");
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
 
 app.Run();
